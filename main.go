538e87371f4b9f8f1ec2a86f21b2112268ea6ee1
Dev-Etto <jhontdsmit36@gmail.com>
2025-04-19 03:12:02 -0300

    feat: add custom logger in application.

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-import "github.com/Dev-Etto/job-board-api/router"
+import (
+	"github.com/Dev-Etto/job-board-api/config"
+	"github.com/Dev-Etto/job-board-api/router"
+)
 
 
-
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
