7c12a229791d12eeb440589b87661fb2d3a42848
Dev-Etto <jhontdsmit36@gmail.com>
2025-04-19 13:39:18 -0300

    feat: add request and response helpers

---
+++ handler/request.go
@@ -0,0 +1,51 @@
+package handler
+
+import "fmt"
+
+type CreateOpeningRequest struct {
+	Role string `json:"role"`
+	Company string `json:"company"`
+	Location string `json:"location"`
+	Remote *bool `json:"remote"`
+	Link string `json:"link"`
+	Salary int64 `json:"salary"`
+}
+
+
+func errParamIsRequered(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
+
+
+func(request *CreateOpeningRequest) Validate() error {
+
+	if request.Role == "" && request.Company == "" && request.Link == "" && request.Location == "" && request.Remote == nil && request.Salary <= 0 {
+		return fmt.Errorf("malformed request body")
+	}
+
+	if request.Role == "" {
+		return errParamIsRequered("role", "string")
+	}
+
+	if request.Company == "" {
+		return errParamIsRequered("company", "string")
+	}
+
+	if request.Location == "" {
+		return errParamIsRequered("location", "string")
+	}
+
+	if request.Link == "" {
+		return errParamIsRequered("link", "string")
+	}
+
+	if request.Remote == nil {
+		return errParamIsRequered("remote", "string")
+	}
+
+	if request.Salary <= 0  {
+		return errParamIsRequered("salary", "string")
+	}
+
+	return nil
+}
